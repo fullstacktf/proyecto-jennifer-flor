13a86a4820889a226b39f91ed02671d555ba5d48
Jennifer Morales <jemoraldi@gmail.com>
2020-05-02 22:20:15 +0100

    Navigation drawer components

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -8,7 +8,25 @@
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
     </v-app-bar>
-    <v-navigation-drawer v-model="drawer" app class="grey lighten-3">
+    <v-navigation-drawer v-model="drawer" app color="grey lighten-3">
+      <v-list>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          router
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <div class="pa-2">
+        <v-btn block color="accent" depressed>Cerrar sesión</v-btn>
+      </div>
     </v-navigation-drawer>
   </nav>
 </template>
@@ -17,7 +35,13 @@
 export default {
   data() {
     return {
-      drawer: false
+      drawer: false,
+      items: [
+        { icon: 'mdi-view-dashboard', title: 'Dashboard', to: '/' },
+        { icon: 'mdi-account', title: 'Perfil', to: '/profile' },
+        { icon: 'mdi-car', title: 'Alquileres', to: '/rent' },
+        { icon: 'mdi-cog', title: 'Configuración', to: '/settings' }
+      ]
     }
   }
 }
