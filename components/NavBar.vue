6d59c0ea2e47d814ee6d695fbc117c8d4538b9dc
Jennifer Morales <jemoraldi@gmail.com>
2020-05-03 18:55:49 +0100

    Search button

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -4,10 +4,10 @@
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>Aparka</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn icon nuxt to="/search">
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
       <notification-menu></notification-menu>
+      <v-btn class="ml-3" outlined nuxt to="/search">
+        <v-icon left>mdi-magnify</v-icon>Buscar
+      </v-btn>
     </v-app-bar>
     <v-navigation-drawer v-model="drawer" app color="grey lighten-3">
       <v-list>
